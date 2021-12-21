b84433d65c3829fbd8480ad43c66bddc08f369cb
Neelima <neelima.malurgmail.com>
2021-12-21 11:32:00 +0530

    Initial commit

---
+++ src/main/java/igatorpackage/MyCompany.java
@@ -0,0 +1,36 @@
+package igatorpackage;
+
+public class MyCompany {
+	
+	private String name;
+	private String address;
+	private String company;
+	private int salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
